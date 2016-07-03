8fbc3448dcf5a64626c6839f63a79d074968c152
kikoskyblue <yexinhualove@qq.com>
2016-07-03 22:00:00 +0800

    定义redis工具类

--- src/main/java/cn/com/fml/config/RedisConfig.java
+++ src/main/java/cn/com/fml/config/RedisConfig.java
@@ -1,8 +1,13 @@
 package cn.com.fml.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 /**
@@ -16,10 +21,28 @@ public class RedisConfig {
 	private String ip;
 	
 	@Value("${fml.redis.port:6379}")
-	private String port;
+	private int port;
 	
+	@Bean
 	public JedisPoolConfig jedisPoolConfig(){
 		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
 		return jedisPoolConfig;
+	}
+	
+	@Bean
+	public JedisConnectionFactory jedisFactory(@Qualifier("jedisPoolConfig") JedisPoolConfig poolConfig){
+		JedisConnectionFactory jedisFactory = new JedisConnectionFactory(poolConfig);
+		jedisFactory.setHostName(ip);
+		jedisFactory.setPort(port);
+		JedisPool jedisPool = new JedisPool();
+		return jedisFactory;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Bean
+	public RedisTemplate redisTemplate(@Qualifier("jedisFactory") JedisConnectionFactory jedisFactory){
+		RedisTemplate redisTemplate = new RedisTemplate();
+		redisTemplate.setConnectionFactory(jedisFactory);
+		return redisTemplate;
 	}
 }
